3bea5f52588760767a2f0446ce128988744e48da
Martin <martin.karl.bittner@gmail.com>
2021-11-16 22:50:15 +0100

    save commit 16.11.

---
+++ vowel_replace_function/vowel_replace.js
@@ -0,0 +1,13 @@
+
+
+// Vowel replace. Replace all vowels of a string with one vowel, like this:
+
+// vowelReplace("Contrabass") --> "Cintribiss"
+
+function vowelReplace(str) {
+    str = str.toLowerCase();
+    let newArray = str.split(' ');
+    console.log(newArray);
+}
+
+vowelReplace("Hello")
