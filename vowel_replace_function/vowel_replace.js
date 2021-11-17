16a8bf56ce053b46363c77360190e762b9937c5a
Martin <martin.karl.bittner@gmail.com>
2021-11-17 17:08:24 +0100

    vowel replace functions added

--- vowel_replace_function/vowel_replace.js
+++ vowel_replace_function/vowel_replace.js
@@ -1,14 +1,39 @@
-
-
 // Vowel replace. Replace all vowels of a string with one vowel, like this:
 
 // vowelReplace("Contrabass") --> "Cintribiss"
 
-function vowelReplace(str) {
-    let re = /[\W]/g;
-    let newString = str.toLowerCase().replace(re, '');;
-    let newArray = newString.split('');
-    console.log(newArray);
+function vowelReplace(str, replacement) {
+  let reg = /[aeiou]/g;
+  let newString = str.toLowerCase().replaceAll(reg, replacement);
+
+  return newString.charAt(0).toUpperCase() + newString.slice(1);
 }
 
-vowelReplace("Hello")
+console.log(vowelReplace("Drei Bassisten mit dem Kontrabass", "i"));
+
+
+
+
+
+
+function replaceVowels(str, replacement) {
+  let newString = str.toLowerCase();
+  let newerArray = newString.split("");
+
+  newerArray.map(function (item, i) {
+    if (
+      item === "a" ||
+      item === "e" ||
+      item === "i" ||
+      item === "o" ||
+      item === "u"
+    )
+      newerArray[i] = replacement;
+  });
+
+  return (
+    newerArray.join("").charAt(0).toUpperCase() + newerArray.join("").slice(1)
+  );
+}
+
+console.log(replaceVowels("Drei Bassisten mit dem Kontrabass", "i"));
