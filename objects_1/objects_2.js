00bcc667c63d0c7a18d0614c4c6f918ea97ba9a9
Martin <martin.karl.bittner@gmail.com>
2021-11-18 23:19:00 +0100

    save commit

---
+++ objects_1/objects_2.js
@@ -0,0 +1,64 @@
+//List of highest peaks: 8000+m above sea level.
+
+const highestPeaks = [
+  {
+    name: "Chomolungma (Mt.Everest)",
+    height: 8848.86,
+  },
+  {
+    name: "K2 (Chhogori)",
+    height: 8611,
+  },
+  {
+    name: "Kangchenjunga",
+    height: 8586,
+  },
+  {
+    name: "Lhotse",
+    height: 8516,
+  },
+  {
+    name: "Makalu",
+    height: 8485,
+  },
+  {
+    name: "Cho Oyu",
+    height: 8188,
+  },
+  {
+    name: "Dhaulagiri",
+    height: 8167,
+  },
+  {
+    name: "Manaslu",
+    height: 8163,
+  },
+  {
+    name: "Nanga Parbat",
+    height: 8126,
+  },
+  {
+    name: "Annapurna I",
+    height: 8091,
+  },
+  {
+    name: "Gasherbrum I",
+    height: 8080,
+  },
+  {
+    name: "Broad Peak (Falchan Kangri)",
+    height: 8051,
+  },
+  {
+    name: "Gasherbrum II",
+    height: 8035,
+  },
+  {
+    name: "Shishapangma",
+    height: 8027,
+  },
+];
+
+console.log(highestPeaks);
+
+  
