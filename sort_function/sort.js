d5c091d8cab41643c563c70d7f5b74301748a2ea
Martin <martin.karl.bittner@gmail.com>
2021-12-07 10:06:50 +0100

    **new sort functions added sort numbers in array

--- sort_function/sort.js
+++ sort_function/sort.js
@@ -59,3 +59,51 @@ function newOrder(str)
 return str.split('').sort().join('');
   }
 console.log(newOrder("Fischer Fritze fischt frische Fische"));
+
+
+
+//3
+const numArray = [11, 4, 6, 77, 9, 0, 12, 13, 1, 2]
+///use numeric sorting method in callback function
+numArray.sort(function (a, b) {
+  return a - b
+})
+console.log(numArray);
+
+///reverse: 
+
+numArray.sort(function (a, b) {
+  return b - a
+})
+console.log(numArray);
+
+/* callingArray.sort(function(element1,element2) {
+    ///some conditional logic
+    return a number that is either (<0) OR (>0) OR (0)
+}) 
+
+if we return 0 in the callback func, it means element1 and element2 are the same or should be organized the same level
+---> both elements stay in their position
+
+if we return number >0 : the element 2 should be before element1
+---> the element1 will move to the right of the other element2
+
+
+if we return number <0 : the element1 should be before element2
+---> the element2 will move to the right of element1
+
+the numbers get converted to unicode by an algorithm */
+
+const numArray2 = [17, 4, 6, 77, 99, 8, 102, 4359]
+
+numArray2.sort(function (element1, element2) {
+    if (element1 > element2) {
+        return 1
+    } else if (element1 < element2) {
+        return -1
+    } else {
+        return 0
+    }
+})
+
+console.log(numArray2);
